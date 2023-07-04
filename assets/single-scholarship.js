107ffff6f67c657af3b71c2ef3f283d766adf49f
Mohammedrezq <mohammedrezq2000@gmail.com>
2023-07-04 21:34:03 +0200

    Aded some check for JS Code

--- assets/single-scholarship.js
+++ assets/single-scholarship.js
@@ -1,74 +1,87 @@
 
 jQuery(document).ready(function($) {
     // Get the value of the hidden input
-    const eligibleCountries = $('.gs-scholarship-eligible-countries').val();
-  
-    // Split the list of countries into an array
-    const countries = eligibleCountries.trim().split(', ');
-  
-    // Display the first 3 countries
-    const firstThreeCountries = countries.slice(0, 3);
-    $('.gs-scholarship-nationalities').text(firstThreeCountries.join(', '));
-  
-    // Add a click event listener to the toggle link
-    $('.gs-scholarship-nationalities-container #toggle-link').click(function(event) {
-      event.preventDefault();
+    if($('.gs-scholarship-eligible-countries')) {
 
-    // Check whether to show more or show less
-    if ($(this).text() === 'Show more') {
-        // Display all the countries
-        $('.gs-scholarship-nationalities').text(countries.join(', '));
-  
-        // Update the link text
-        $(this).text('Show less');
-        $(this).parent().find('.ellipsis').removeClass('show');
-        $(this).parent().find('.ellipsis').addClass('hide');
-    } else {
-        // Display only the first 3 countries
+      const eligibleCountries = $('.gs-scholarship-eligible-countries').val();
+    
+      // Split the list of countries into an array
+      if(eligibleCountries){
+
+        const countries = eligibleCountries.trim().split(', ');
+      
+        // Display the first 3 countries
+        const firstThreeCountries = countries.slice(0, 3);
         $('.gs-scholarship-nationalities').text(firstThreeCountries.join(', '));
-  
-        // Update the link text
-        $(this).text('Show more');
-        $(this).parent().find('.ellipsis').removeClass('hide');
-        $(this).parent().find('.ellipsis').addClass('show');
+      
+        // Add a click event listener to the toggle link
+        $('.gs-scholarship-nationalities-container #toggle-link').click(function(event) {
+          event.preventDefault();
+    
+        // Check whether to show more or show less
+        if ($(this).text() === 'Show more') {
+            // Display all the countries
+            $('.gs-scholarship-nationalities').text(countries.join(', '));
+      
+            // Update the link text
+            $(this).text('Show less');
+            $(this).parent().find('.ellipsis').removeClass('show');
+            $(this).parent().find('.ellipsis').addClass('hide');
+        } else {
+            // Display only the first 3 countries
+            $('.gs-scholarship-nationalities').text(firstThreeCountries.join(', '));
+      
+            // Update the link text
+            $(this).text('Show more');
+            $(this).parent().find('.ellipsis').removeClass('hide');
+            $(this).parent().find('.ellipsis').addClass('show');
+          }
+        });
       }
-    });
+    }
+
   });
 
 jQuery(document).ready(function($) {
     // Get the value of the hidden input
-    const eligibleSubjects = $('.gs-scholarship-eligible-subjects').val();
-  
-    // Split the list of subjects into an array
-    const subjects = eligibleSubjects.trim().split(', ');
-  
-    // Display the first 3 subjects
-    const firstThreeSubjects = subjects.slice(0, 3);
-    $('.gs-scholarship-subjects').text(firstThreeSubjects.join(', '));
-  
-    // Add a click event listener to the toggle link
-    $('.gs-scholarship-subjects-container #toggle-link').click(function(event) {
-      event.preventDefault();
-  
-    // Check whether to show more or show less
-    if ($(this).text() === 'Show more') {
-        // Display all the subjects
-        $('.gs-scholarship-subjects').text(subjects.join(', '));
-  
-        // Update the link text
-        $(this).text('Show less');
-        $(this).parent().find('.ellipsis').removeClass('show');
-        $(this).parent().find('.ellipsis').addClass('hide');
-    } else {
-        // Display only the first 3 subjects
+    if($('.gs-scholarship-eligible-subjects')) {
+
+      const eligibleSubjects = $('.gs-scholarship-eligible-subjects').val();
+    
+      if(eligibleSubjects) {
+
+        // Split the list of subjects into an array
+        const subjects = eligibleSubjects.trim().split(', ');
+      
+        // Display the first 3 subjects
+        const firstThreeSubjects = subjects.slice(0, 3);
         $('.gs-scholarship-subjects').text(firstThreeSubjects.join(', '));
-  
-        // Update the link text
-        $(this).text('Show more');
-        $(this).parent().find('.ellipsis').removeClass('hide');
-        $(this).parent().find('.ellipsis').addClass('show');
+      
+        // Add a click event listener to the toggle link
+        $('.gs-scholarship-subjects-container #toggle-link').click(function(event) {
+          event.preventDefault();
+      
+        // Check whether to show more or show less
+        if ($(this).text() === 'Show more') {
+            // Display all the subjects
+            $('.gs-scholarship-subjects').text(subjects.join(', '));
+      
+            // Update the link text
+            $(this).text('Show less');
+            $(this).parent().find('.ellipsis').removeClass('show');
+            $(this).parent().find('.ellipsis').addClass('hide');
+        } else {
+            // Display only the first 3 subjects
+            $('.gs-scholarship-subjects').text(firstThreeSubjects.join(', '));
+      
+            // Update the link text
+            $(this).text('Show more');
+            $(this).parent().find('.ellipsis').removeClass('hide');
+            $(this).parent().find('.ellipsis').addClass('show');
+          }
+        });
       }
-    });
+    }
   });
 
 // DOM content loaded
@@ -127,28 +140,34 @@ function getFeebackForm() {
   const thankYouMessage = document.createElement('p'); // new line
   thankYouMessage.innerHTML = 'Thank you for your feedback.'; // new line
 
-  yesBtn.addEventListener('click', function() {
-    buttonsDiv.style.display = "flex";
-    document.querySelector('.step-2').style.display = "none";
-  });
-
-  noBtn.addEventListener('click', function() {
-    buttonsDiv.style.display = "flex";
-    document.querySelector('.step-2').style.display = "block";
-    for (var i = 0; i < radioInputs.length; i++) {
-      radioInputs[i].parentElement.style.display = "block";
-    }
-    otherTextarea.style.display = "block";
-  });
-
-  for (var i = 0; i < radioInputs.length; i++) {
-    radioInputs[i].addEventListener('click', function() {
-      if (this.value === "other") {
-        otherTextarea.style.display = "block";
-      } else {
-        otherTextarea.style.display = "none";
-      }
+  if(yesBtn) {
+    yesBtn.addEventListener('click', function() {
+      buttonsDiv.style.display = "flex";
+      document.querySelector('.step-2').style.display = "none";
     });
+  }
+
+  if(noBtn) {
+    noBtn.addEventListener('click', function() {
+      buttonsDiv.style.display = "flex";
+      document.querySelector('.step-2').style.display = "block";
+      for (var i = 0; i < radioInputs.length; i++) {
+        radioInputs[i].parentElement.style.display = "block";
+      }
+      otherTextarea.style.display = "block";
+    });
+  }
+
+  if( radioInputs && radioInputs.length > 0 ) {
+    for (var i = 0; i < radioInputs.length; i++) {
+      radioInputs[i].addEventListener('click', function() {
+        if (this.value === "other") {
+          otherTextarea.style.display = "block";
+        } else {
+          otherTextarea.style.display = "none";
+        }
+      });
+    }
   }
 
     jQuery( '[name="submit"]' ).click( function(e) {
