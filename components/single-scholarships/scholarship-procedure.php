c2443a60046be20df0b7715c33a4b196d9c905cb
Mohammedrezq <mohammedrezq2000@gmail.com>
2023-07-05 17:49:42 +0200

    Fixes for Procedures and Steps to show one if the other is not there, so it doesn't have to have both to show some lists !

--- components/single-scholarships/scholarship-procedure.php
+++ components/single-scholarships/scholarship-procedure.php
@@ -18,25 +18,29 @@
             For <?php echo $scholarship_title; ?>, you DO NOT need to apply separately. That is, once you’ve submitted your admission application, you are automatically considered for the scholarship, as long as you meet the eligibility criteria. 
             </p>
             <p>If you need more information regarding the scholarship application process, visit the official <a href="<?php echo $scholarship_page_link; ?>">scholarship page</a>.</p> -->
-        <?php elseif( $separate_application == "Yes" &&  ( NULL != get_field('application_procedure') && !empty(get_field('application_procedure')) ) && ( NULL != get_field('additional_scholarship_requirements') &&  !empty( get_field('additional_scholarship_requirements')) )  ) : 
+        <?php elseif( $separate_application == "Yes"  ) : 
+
+         if( ( NULL != get_field('additional_scholarship_requirements') &&  !empty( get_field('additional_scholarship_requirements')) )) : 
             ?>
 
-        <h3><?php echo $scholarship_title; ?> Application Process</h3>
+            <h3><?php echo $scholarship_title; ?> Application Process</h3>
             <p>A separate scholarship application – in addition to the university admission – is needed for <?php echo $scholarship_title; ?>. You’ll have to collect these requirements:</p>
             <ul class="fa-ul">
                 <?php foreach(get_field('additional_scholarship_requirements') as $requirement) : ?>
                 <li><span class="fa-li"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg></span><b><div class="requirement-item"><?php echo $requirement['requirements']; ?></div></b></li>
                 <?php endforeach; ?>
             </ul>
+            <?php endif; ?>
+            <?php if( NULL != get_field('application_procedure') && !empty(get_field('application_procedure')) ) : ?>
             <p>Once you have those ready, follow these steps to submit your scholarship application and requirements:</p>
             <ol>
                 <?php foreach(get_field('application_procedure') as $step) : ?>
-                    <li><b><?php echo $step['steps']; ?></b></li>
+                    <li></b><?php echo $step['steps']; ?></b></li>
                 <?php endforeach; ?>
             </ol>
             
+            <?php endif; ?>
             <p>If you need more information regarding the scholarship application process, visit the official <a href="<?php echo $scholarship_page_link; ?>">scholarship page</a>.</p>
-
 
             <?php elseif($separate_application == "Yes" &&( NULL == get_field('application_procedure') && empty(get_field('application_procedure')) ) && ( NULL == get_field('additional_scholarship_requirements') &&  empty( get_field('additional_scholarship_requirements')) ) ): ?>
                 <p>A separate scholarship application is needed for this offer. You may check the <b><a href="<?php echo $scholarship_page_link; ?>"><?php echo $scholarship_title ?> page</a></b> for more details, including application requirements and procedures.</p>
