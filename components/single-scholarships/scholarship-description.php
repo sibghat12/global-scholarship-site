1fe4642c5bc5870ceddce130ba9d4abbfda0a05b
Mohammedrezq <mohammedrezq2000@gmail.com>
2023-07-15 00:16:04 +0200

    Some change for text according to new instructions for eligible criteria and scholarship description

--- components/single-scholarships/scholarship-description.php
+++ components/single-scholarships/scholarship-description.php
@@ -1,4 +1,4 @@
 <p>
-Do you want a scholarship in <?php echo $country_name; ?>? Consider studying at <?php echo $institution_name; ?>, a <?php echo strtolower($institution_type); ?> institution founded in <?php echo $founded_year; ?> and located in <?php echo $city_name . ", " . $country_name; ?>. This institution offers scholarships, which includes <?php echo $scholarship_title; ?>, one of the <?php echo get_adjective_scholarship_amount($scholarship_type);?> scholarships for <?php 
+Do you want a scholarship in <?php echo $country_name; ?>? Consider studying at <?php echo $institution_name; ?>, a <?php echo strtolower($institution_type); ?> institution founded in <?php echo $founded_year; ?> and located in <?php echo $city_name . ", " . $country_name; ?>. <?php echo $institution_name; ?> offers <?php echo $scholarship_title; ?>, one of the <?php echo get_adjective_scholarship_amount($scholarship_type);?> scholarships for <?php 
 echo ( str_contains($degrees_text, 'PhD') ) ? str_replace('PhD', 'Ph.D.', $degrees_text) : $degrees_text; ?> students.
 </p>
