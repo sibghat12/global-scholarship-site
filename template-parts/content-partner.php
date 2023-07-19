c040f535e272ee5dfa25067c01ce59b922b0f72d
Mohammedrezq <mohammedrezq2000@gmail.com>
2023-07-20 00:46:10 +0200

    Initial Additional of Copy of SAA Hero Section into GS Partner With Us Page

--- template-parts/content-partner.php
+++ template-parts/content-partner.php
@@ -1,14 +1,15 @@
 <?php 
 
-$home_page_id = get_the_ID();
-$hero_section_title = get_field( 'hero_section_title', $home_page_id );
-$hero_section_heading = get_field( 'hero_section_heading', $home_page_id );
-$hero_section_text = get_field( 'hero_section_text', $home_page_id );
-$hero_section_image = get_field( 'hero_section_image', $home_page_id );
-$hero_section_button_title = get_field( 'hero_section_button_title', $home_page_id );
-$hero_section_button = get_field( 'hero_section_button', $home_page_id );
-$hero_section_image_id = $hero_section_image ?? $hero_section_image['ID'];
+$partner_with_us_page_id = get_the_ID();
+$hero_section_title = get_field( 'hero_section_title', $partner_with_us_page_id );
+$hero_section_heading = get_field( 'hero_section_heading', $partner_with_us_page_id );
+$hero_section_text = get_field( 'hero_section_text', $partner_with_us_page_id );
+$hero_section_image = get_field( 'hero_section_image', $partner_with_us_page_id );
+$hero_section_button_title = get_field( 'hero_section_button_title', $partner_with_us_page_id );
+$hero_section_button = get_field( 'hero_section_button', $partner_with_us_page_id );
+$hero_section_image_id = $hero_section_image['ID'];
 
+// GS Hero Section Partner With Us
 
 require get_stylesheet_directory() . '/components/partner-with-us/hero.php';
 
