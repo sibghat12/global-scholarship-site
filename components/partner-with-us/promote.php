c1865f628f4b11a6238a9fce56d53b508f3e4bd5
Mohammedrezq <mohammedrezq2000@gmail.com>
2023-07-21 03:47:12 +0200

    Added Boxes What to promote

--- components/partner-with-us/promote.php
+++ components/partner-with-us/promote.php
@@ -9,15 +9,23 @@
     </div>
     <div class="gs-partner-what-promote-boxes-conatiner">
         <div class="gs-partner-what-promote-box-program gs-promote-box" data-promote-box="1">
-            
-
+            <div class="gs-promote-box-content">
+                <h3 class="gs-promote-box-title">Promote your university programme</h3>
+                <p class="gs-promote-box-text">You can showcase all your programs to a global audience of international students</p>
+            </div>
         </div>
         <div class="gs-partner-what-promote-box-course gs-promote-box" data-promote-box="2">
-
+        <div class="gs-promote-box-content">
+                <h3 class="gs-promote-box-title">Promote a single course</h3>
+                <p class="gs-promote-box-text">We offer the flexibility to promote a single program, giving it the traction it deserves among students seeking niche educational paths</p>
+            </div>
 
         </div>
         <div class="gs-partner-what-promote-box-department gs-promote-box" data-promote-box="3">
-
+        <div class="gs-promote-box-content">
+                <h3 class="gs-promote-box-title">Promote an entire departement</h3>
+                <p class="gs-promote-box-text">You can also promote  an entire department, highlighting the breadth and depth of academic prospects available within that discipline</p>
+            </div>
 
         </div>
     </div>
