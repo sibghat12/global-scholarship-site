b1ecebd0bf297a1d65f827822e1229b8cac3a7c3
Mohammedrezq <mohammedrezq2000@gmail.com>
2023-08-16 18:11:14 +0200

    Commented the Popular Scholarship in country again

--- components/single-scholarships/scholarship-outro.php
+++ components/single-scholarships/scholarship-outro.php
@@ -4,36 +4,36 @@
     <p>Want to learn more about <?php echo $institution_name; ?>, <?php echo $scholarship_title; ?>, requirements, application process, and other related information? Check the <a href="<?php echo $scholarship_page_link; ?>"><?php echo $scholarship_title; ?> page</a>!</p>
 
     
-    <h2>Popular Scholarships in  <?php echo $country_name; ?></h2> 
+    <!-- <h2>Popular Scholarships in  <?php echo $country_name; ?></h2>  -->
 
     <?php
 
-    $country_institutions = get_institutions_location($country_name);
+    // $country_institutions = get_institutions_location($country_name);
 
-    $country_institutions_ids = $country_institutions->posts;
+    // $country_institutions_ids = $country_institutions->posts;
 
     
-    $best_scholarships_by_country = [];
+    // $best_scholarships_by_country = [];
     
-    foreach($country_institutions_ids as $id ) {
-        $scholarships = get_scholarships_by_weight($id); // Assuming there is a function to retrieve scholarships by weight for a given institution ID.
+    // foreach($country_institutions_ids as $id ) {
+        // $scholarships = get_scholarships_by_weight($id); // Assuming there is a function to retrieve scholarships by weight for a given institution ID.
         
-        $best_scholarships_by_country = array_merge($best_scholarships_by_country, $scholarships->posts);
+        // $best_scholarships_by_country = array_merge($best_scholarships_by_country, $scholarships->posts);
         
-    }
+    // }
 
     // We only get the Top 5 Scholarships by Scholarship Weight
-    $best_scholarships_by_country = array_slice( $best_scholarships_by_country, 0, 5);
-    ?>
+    // $best_scholarships_by_country = array_slice( $best_scholarships_by_country, 0, 5);
+    // ?>
     <!-- Display Popular Scholarships in Country --->
 
-    <div class="gs-popular-scholarships-container">
-        <?php foreach($best_scholarships_by_country as $key => $scholarship) : ?>
-            <div class="gs-popular-scholarship-item">
-                <a href="<?php echo get_the_permalink($scholarship) ?>"><?php echo get_the_title($scholarship); ?></a>
-            </div>
-        <?php endforeach ?>
-    </div>
+    <!-- <div class="gs-popular-scholarships-container"> -->
+        <!-- <?php foreach($best_scholarships_by_country as $key => $scholarship) : ?> -->
+            <!-- <div class="gs-popular-scholarship-item"> -->
+                <!-- <a href="<?php echo get_the_permalink($scholarship) ?>"><?php echo get_the_title($scholarship); ?></a> -->
+            <!-- </div> -->
+        <!-- <?php endforeach ?> -->
+    <!-- </div> -->
 
 </section>
 
