dfc81bd8c20c7dd4429e554ab5a544154c6ccc02
Mohammedrezq <mohammedrezq2000@gmail.com>
2023-08-18 18:07:34 +0200

    Added code to retrieve the most popular scholarships in country using scholarship_weights ACF

--- components/single-scholarships/scholarship-outro.php
+++ components/single-scholarships/scholarship-outro.php
@@ -4,36 +4,23 @@
     <p>Want to learn more about <?php echo $institution_name; ?>, <?php echo $scholarship_title; ?>, requirements, application process, and other related information? Check the <a href="<?php echo $scholarship_page_link; ?>"><?php echo $scholarship_title; ?> page</a>!</p>
 
     
-    <!-- <h2>Popular Scholarships in  <?php echo $country_name; ?></h2>  -->
+    <h2>Popular Scholarships in  <?php echo $country_name; ?></h2> 
 
     <?php
 
-    // $country_institutions = get_institutions_location($country_name);
+    $country_institution = get_field('institution_country');
+    $best_scholarships_country_query = get_best_scholarships_country($country_institution, 5);
 
-    // $country_institutions_ids = $country_institutions->posts;
+    $best_scholarships_ids = $best_scholarships_country_query->get_posts();
+    ?>
 
-    
-    // $best_scholarships_by_country = [];
-    
-    // foreach($country_institutions_ids as $id ) {
-        // $scholarships = get_scholarships_by_weight($id); // Assuming there is a function to retrieve scholarships by weight for a given institution ID.
-        
-        // $best_scholarships_by_country = array_merge($best_scholarships_by_country, $scholarships->posts);
-        
-    // }
-
-    // We only get the Top 5 Scholarships by Scholarship Weight
-    // $best_scholarships_by_country = array_slice( $best_scholarships_by_country, 0, 5);
-    // ?>
-    <!-- Display Popular Scholarships in Country --->
-
-    <!-- <div class="gs-popular-scholarships-container"> -->
-        <!-- <?php foreach($best_scholarships_by_country as $key => $scholarship) : ?> -->
-            <!-- <div class="gs-popular-scholarship-item"> -->
-                <!-- <a href="<?php echo get_the_permalink($scholarship) ?>"><?php echo get_the_title($scholarship); ?></a> -->
-            <!-- </div> -->
-        <!-- <?php endforeach ?> -->
-    <!-- </div> -->
+    <div class="gs-popular-scholarships-container">
+        <?php foreach($best_scholarships_ids as $key => $scholarship) : ?>
+            <div class="gs-popular-scholarship-item">
+                <a href="<?php echo get_the_permalink($scholarship) ?>"><?php echo get_the_title($scholarship); ?></a>
+            </div>
+        <?php endforeach ?>
+    </div>
 
 </section>
 
