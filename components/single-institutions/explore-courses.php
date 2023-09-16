7988379a2172ee444869f0809f69849c51df1e19
Mohammedrezq <mohammedrezq2000@gmail.com>
2023-09-16 03:21:25 +0200

    Added Explore Courses Worldwide

---
+++ components/single-institutions/explore-courses.php
@@ -0,0 +1,5 @@
+<div class="gs-institution-explore-courses">
+    <h1 class="gs-institution-explore-courses-title">Explore Available Courses Worldwide</h1>
+    <p class="gs-institution-explore-courses-text">Do you want to see what programs and courses await you abroad? Then check out our list of open courses from renowned institutions around the world specifically for international students from your country. Choose a course that fits your academic goals, and be mindful of the application deadlines for each open course.</p>
+    <a class="gs-institution-explore-courses-button" href="">Open Courses &raquo;</a>
+</div>
