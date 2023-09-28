52d38c38bd974f205fe8cf60f44fab5c0675ae8e
Mohammedrezq <mohammedrezq2000@gmail.com>
2023-09-28 16:44:52 +0200

    Fixes and enhancements on Stylings of GS institutions

--- components/single-institutions/breadcrumbs.php
+++ components/single-institutions/breadcrumbs.php
@@ -1,6 +1,6 @@
 <div class="gs-institutions-breadcrumbs">
         <a class="gs-institutions-breadcrumb-home" href="<?php echo site_url(); ?>">Home</a> >
-        <a class="gs-institutions-breadcrumb-home" href="<?php echo site_url(); ?>/scholarship-search/<?php echo $hyphenated ?>"><?php echo $country_name ." Scholarships";  ?> </a> >
+        <a class="gs-institutions-breadcrumb-home" href="<?php echo site_url(); ?>/scholarship-search/<?php echo $hyphenated ?>"><?php echo $country_name ." Scholarships";  ?></a> >
         <a class="gs-institutions-breadcrumb-institution" href="#"> <?php  echo $title; ?> </a>
         <br>
 </div>
