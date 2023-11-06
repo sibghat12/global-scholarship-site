6f9920118836c5be221fefe06d5089a3761ad8dd
Mohammedrezq <mohammedrezq2000@gmail.com>
2023-11-06 05:14:49 +0200

    Removed the action function too

--- functions.php
+++ functions.php
@@ -118,7 +118,6 @@ function my_deregister_scripts(){
 
 add_action( 'wp_footer', 'my_deregister_scripts' );
 
-add_action( 'wp_enqueue_scripts', 'custom_disable_theme_js' );
 
 function dequeue_jquery_migrate( $scripts ) {
     if ( ! is_admin() && ! empty( $scripts->registered['jquery'] ) ) {
