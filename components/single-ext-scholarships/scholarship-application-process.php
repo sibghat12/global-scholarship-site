5fea2da3e247196875b03ea69165e2f07b9ab1f0
Mohammedrezq <mohammedrezq2000@gmail.com>
2024-01-19 10:16:52 +0200

    Improvements on GS application process and other points

--- components/single-ext-scholarships/scholarship-application-process.php
+++ components/single-ext-scholarships/scholarship-application-process.php
@@ -2,20 +2,21 @@
 
     <h2><?php echo $scholarship_title; ?> Application Process</h2>
 
-    <p>In this section, we will be discussing the requirements and step-by-step process on how to apply for <?php echo $scholarship_title; ?>:</p>
+    <p>In this section, we'll provide a clear general overview of requirements and steps to guide you through the application process for <?php echo $scholarship_title; ?>.</p>
+
     <div class="gs-ext-schoalrship-application-requirements">
 
         <h3><?php echo $scholarship_title; ?> Application Requirements:</h3>
 
-        <p>To start your application for <?php echo $scholarship_title; ?>, you need to submit the following documents and meet the following requirements:</p>
+        <p>To begin your application for <?php echo $scholarship_title; ?>, you'll need to prepare and submit the following documents:</p>
         <?php // Scholarship Application Steps ?>
 
         <?php if($application_steps) : ?>
-        <ol>
-            <?php foreach($application_steps as $step) : ?>
-            <li><?php echo $step['steps']; ?></li>
-            <?php endforeach; ?>
-        </ol>
+            <ul class="fa-ul">
+                <?php foreach($application_steps as $step) : ?>
+                    <li><span class="fa-li"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg></span><div class="step-item"><?php echo $step['steps']; ?></div></li>
+                <?php endforeach; ?>
+            </ul>
         <?php endif; ?>
     </div>
     <div class="gs-ext-schoalrship-application-procedures">
