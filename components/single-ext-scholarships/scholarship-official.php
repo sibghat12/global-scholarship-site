8385f2a3f9308a30d6ce497d0f477493dbcf092a
Mohammedrezq <mohammedrezq2000@gmail.com>
2024-01-23 02:26:45 +0200

    Official Page

--- components/single-ext-scholarships/scholarship-official.php
+++ components/single-ext-scholarships/scholarship-official.php
@@ -1,7 +1,7 @@
 <section id="ext-scholarship-official" class="gs-ext-scholarship-official">
 
     <div class="gs-ext-schoalrship-official-container">
-        <h2><?php echo $scholarship_title ?></h2>
+        <h2><?php echo $scholarship_title ?> Official Page</h2>
     
         <p>With the help of <?php echo $scholarship_title ?>, you’ll surely be able to support your studies abroad! To learn more about <?php echo $scholarship_title ?>, and its coverage, application process and deadlines, make sure to visit their <a href="<?php echo $scholarship_page_link; ?>" target="_blank">official page!</a></p>
     </div>
